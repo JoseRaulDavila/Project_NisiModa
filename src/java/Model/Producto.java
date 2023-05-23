c8014ba5b57c83a81543a542f31a60afbdd94e4d
Luis Perez <luisPE4SQL@outlook.com>
2023-05-22 20:20:45 -0500

    Primer avance: Creación y conexión con la Base de datos y la creación de la lista de productos

---
+++ src/java/Model/Producto.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author U20207297
+ */
+public class Producto {
+    int id;
+    String nombres;
+    int cantidad;
+    String estado;
+    InputStream foto;
+    String descripcion;
+    int precio;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombres, int cantidad, String estado, InputStream foto, String descripcion, int precio) {
+        this.id = id;
+        this.nombres = nombres;
+        this.cantidad = cantidad;
+        this.estado = estado;
+        this.foto = foto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public InputStream getFoto() {
+        return foto;
+    }
+
+    public void setFoto(InputStream foto) {
+        this.foto = foto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+}
